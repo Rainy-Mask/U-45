cb11e2ef00ef981f39ec11b10510457e812f3cf1
ploidi <ybozbay17@gmail.com>
2023-06-21 11:57:13 +0300

    Camera Fix

--- Assets/Scripts/Character/MouseLook.cs
+++ Assets/Scripts/Character/MouseLook.cs
@@ -1,26 +1,54 @@
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 
 public class MouseLook : MonoBehaviour
 {
-    public float sensivity = 100f;
+    public float sensitivity = 100f;
     public Transform body;
+    public float jumpCameraOffset = 0.5f; // Zıplama animasyonunda kamera yukarı kaydırma miktarı
+    public float cameraResetSpeed = 2f; // Kamera resetleme hızı
+
     private float xRot = 0;
+    private float originalCameraHeight;
+    private bool isJumping = false;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        originalCameraHeight = transform.localPosition.y; // Kameranın orijinal yüksekliği
     }
 
     private void Update()
     {
-        float hor = Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
-        float ver = Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+        float hor = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float ver = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
         xRot -= ver;
         xRot = Mathf.Clamp(xRot, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRot, 0, 0);
         body.Rotate(Vector3.up * hor);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (isJumping)
+        {
+            Vector3 jumpCameraPosition = transform.localPosition; // Kameranın pozisyonu alınır
+            jumpCameraPosition.y = originalCameraHeight + jumpCameraOffset; // Kameranın yüksekliği artırılır
+            transform.localPosition = jumpCameraPosition; // Kameranın pozisyonu güncellenir
+
+            isJumping = false;
+        }
+        else
+        {
+            Vector3 resetCameraPosition = transform.localPosition; // Kameranın pozisyonu alınır
+            resetCameraPosition.y = originalCameraHeight; // Kameranın yüksekliği sıfırlanır
+            transform.localPosition = Vector3.Lerp(transform.localPosition, resetCameraPosition, cameraResetSpeed * Time.deltaTime); // Kameranın pozisyonu eski haline doğru yumuşak bir geçiş yapar
+        }
     }
 }
-
